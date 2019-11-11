ad8c9d93da56baae7c3df3d8bf52f44571b797cb
Evan Brierton <evanbrierton@icloud.com>
2019-11-11 23:34:45 +0000

    Add firebase auth

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,30 +1,51 @@
 import React from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import { func, shape, string } from 'prop-types';
+import Button from '@material/react-button';
+
+import '../../assets/SASS/index.scss';
+import firebaseConfig from '../../firebaseConfig';
 import logo from '../../assets/images/logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
 
-export default App;
+const providers = { googleProvider: new firebase.auth.GoogleAuthProvider() };
+
+const App = ({ user, signOut, signInWithGoogle }) => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      {user ? (
+        <p>
+          Hello,
+          {' '}
+          {user.displayName}
+        </p>
+      ) : <p>Please sign in.</p>}
+      {user ? (
+        <Button outlined onClick={signOut} className="button-alternate">
+          Sign Out
+        </Button>
+      ) : (
+        <Button outlined onClick={signInWithGoogle} className="button-alternate">
+          Sign in With Google
+        </Button>
+      )}
+    </header>
+  </div>
+);
+
+App.propTypes = {
+  user: shape({ displayName: string, email: string }),
+  signOut: func.isRequired,
+  signInWithGoogle: func.isRequired,
+};
+
+App.defaultProps = { user: null };
+
+
+export default withFirebaseAuth({ providers, firebaseAppAuth })(App);
